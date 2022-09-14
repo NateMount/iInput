bd23220213f0db1c54d3306cba9a8785858dee8f
Nate Mount <ngmount2021@gmail.com>
2022-09-14 09:39:37 -0400

    Converted trie into a more modular class

--- trie.py
+++ trie.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3.10
 
+import sys
+
 class TrieNode:
 	def __init__(self):
 		self.children = {}
@@ -8,6 +10,7 @@ class TrieNode:
 class Trie:
 	def __init__(self):
 		self.root = TrieNode()
+		self.suggestions = []
 	
 	def formTrie(self, keys):
 		for key in keys:
@@ -26,12 +29,12 @@ class Trie:
 	
 	def suggestionRec(self, node, word):
 		if node.isEnd:
-			print(word)
+			self.suggestions.append(word)
 
 		for a, n in node.children.items():
 			self.suggestionRec(n, word + a)
 	
-	def printAutoSugg(self, key):
+	def getAutoSugg(self, key):
 		node = self.root
 		for c in key:
 			if not node.children.get(c):
@@ -42,18 +45,8 @@ class Trie:
 			return -1
 
 		self.suggestionRec(node, key)
+
 		return 1
 
 keys = map(lambda x: x.lower(), open('words.txt','r').read().split('\n'))
-#keys = ['hello','hi','hell','helvetica','homostasis', 'apple', 'app', 'arson', 'zombie']
-t = Trie()
-t.formTrie(keys)
-print(t.root.children)
 
-while True:
-	comp = t.printAutoSugg(input("<< "))
-
-	if comp == -1:
-	    print("No other strings found with this prefix\n")
-	elif comp == 0:
-	    print("No string found with this prefix\n")
